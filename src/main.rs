bd958b75a1b0ece33863f5b8b90bb55bd34d649c
Jazal-UoE <s1909640@ed.ac.uk>
2025-05-28 19:09:02 +0100

    implemented support for multiple guesses

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,29 @@ use std::io;
 
 fn main() {
     println!("Guess the number!");
-    let secret_number: i32 = rand::rng().random_range(1..=100);
+    let secret_number = rand::rng().random_range(1..=100);
 
     println!("The secret number is {}", secret_number);
-    println!("Please input your guses.");
 
-    let mut guess: String = String::new();
+    loop {
+        println!("Please input your guses.");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read the line");
+        let mut guess: String = String::new();
 
-    let guess: i32 = guess.trim().parse().expect("Please type a number!");
-    println!("You guessed {} ", guess);
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read the line");
+
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        println!("You guessed {} ", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
     // priotln!("{result}")
 }
